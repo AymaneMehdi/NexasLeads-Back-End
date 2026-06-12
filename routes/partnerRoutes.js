42552b6b66e4d3c6a4f6647ee6579e6ff19a448c
Aymane Mehdi <yorubornin1999@gmail.com>
2026-06-12 18:08:58 +0100

    Add authentication middleware to partner routes for secure CRUD operations.

--- routes/partnerRoutes.js
+++ routes/partnerRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/partnerController')
+const auth = require("../middlewares/auth");
 
 router.get('/',controller.getAllPartners);
 router.get('/:id',controller.getOnePartner);
-router.post('/',controller.createPartner);
-router.put('/:id',controller.updatePartner);
-router.delete('/:id',controller.deletePartner);
+router.post('/',auth,controller.createPartner);
+router.put('/:id',auth,controller.updatePartner);
+router.delete('/:id',auth,controller.deletePartner);
 
 module.exports = router;
